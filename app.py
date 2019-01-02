99af3db36a6bfeb73dbceb6bd0608f16ac5e7cfe
Jim Doran <jrdoran@us.ibm.com>
2019-01-02 13:42:13 -0500

    ddd

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import os
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 
+#from models import *
+
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
--- manage.py
+++ manage.py
@@ -3,9 +3,11 @@ from flask.ext.script import Manager
 from flask.ext.migrate import Migrate, MigrateCommand
 
 from app import app, db
+#from models import *
 
 
 app.config.from_object(os.environ['APP_SETTINGS'])
+
 
 migrate = Migrate(app, db)
 manager = Manager(app)
